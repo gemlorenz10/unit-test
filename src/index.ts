6a0107624a76771e1c54ee36e822bae43e10d271
gemlorenz10 <gemlorenz10@gmail.com>
2017-12-30 20:49:25 +0800

    code for testing

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-﻿import * as puppeteer from 'puppeteer';
+﻿import { OntueLogin } from './ontue/ontue-login';
+import { getUserJson } from './ontue/ontue-lib/ontue-library';
+import * as userJson from '../data/user-data.json';
+
+let user = getUserJson( userJson )[0]
+console.log( user );
+let login = new OntueLogin(user);
+login.main();
