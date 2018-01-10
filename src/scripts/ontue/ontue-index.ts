cd03730d1a33625a3db4f72f3d8743aaf554dbe1
gemlorenz10 <gemlorenz10@gmail.com>
2018-01-10 22:23:44 +0800

    index test run -> not yet tested

--- src/scripts/ontue/ontue-index.ts
+++ src/scripts/ontue/ontue-index.ts
@@ -1,1 +1,14 @@
-﻿
+﻿import { schedule_data, user_data } from './../../data/test-data';
+import { OntueSchedulePage } from './../lib/ontue-library';
+import { OntueSchedule } from './ontue-crud-schedule';
+
+// Scheduler Test
+let eden = user_data[0];
+let schedule = schedule_data[0]
+let sched_page = new OntueSchedulePage()
+let schedule_test = new OntueSchedule( eden, sched_page, schedule  );
+
+setTimeout(function(){
+    schedule_test.main();
+}, 5000);
+
