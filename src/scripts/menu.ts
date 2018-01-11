4132f2b9f180ca46fa981e0ebea80061a27dd49c
gemlorenz10 <gemlorenz10@gmail.com>
2018-01-11 20:03:24 +0800

    update to run nicely as a module

--- src/scripts/menu.ts
+++ src/scripts/menu.ts
@@ -8,7 +8,7 @@ import { OntueLoginPage } from './lib/ontue-library';
 /**
  * Pass Page LoginPage thats extending MenuPage in the Constructor
  */
-class Menu extends Login {
+export class Menu extends Login {
     private menu_page;
     private menu_user;
     constructor( private _menuUser: IUserInfo, private _pageLoginExtendMenu ) {
@@ -17,17 +17,24 @@ class Menu extends Login {
         this.menu_user = _menuUser;
     }
     /**
-     * Starts a browser then run automation.
+     * Starts a browser then run schedule. Closes the browser afterwards.
+     * @code
+     *  
+     * @endcode
      */
     async main() {
+        if ( !this.page ) await this.startMenu();
+        await this.checkMenuList();
+        await this.checkHeadMenu();
+
+        // this.activitySummary();
+        await this.endScript();
+    }
+
+    async startMenu() {
         console.log('TEST :', this.menu_page.domain );
         await this.start( this.menu_page.domain, browserOption, this.menu_page.sitename ).catch( async e => await this.fatal('fail-webpage', `Can't open ${this.menu_page.domain}!`) );
         if ( this.menu_user ) await this.submitLogin();
-        await this.checkMenuList();
-        // await this.checkHeadMenu();
-
-        this.activitySummary();
-        await this.exitProgram(0);
     }
 
     /**
@@ -35,37 +42,42 @@ class Menu extends Login {
      *
      */
     async checkMenuList() {
+
+        if( !this.page ) await this.startMenu();
+        console.log( 'TEST MENU PAGE' )
         let re;
         let menu_list = ( this.menu_user )
                         ? this.menu_page.menuExpectListLogin()
                         : this.menu_page.menuExpectList();
-
+        let i = 0;
         for ( re of  menu_list) {
-            console.log('TEST:',re);
-            await this.open(this.menu_page.head_menu, [re.menu], null, `Page failed to open when clicking -> ${this.menu_page.head_menu}`);
-            await this.open(re.menu, [re.expect], null, `Page failed to open when clicking -> ${re.menu}`);
+            console.log(`TEST ${i}:`,re);
+            await this.open(this.menu_page.head_menu, [re.menu], { idx: i, error_message : `Page failed to open when clicking -> ${this.menu_page.head_menu}`});
+            await this.open(re.menu, [re.expect],  { idx : i, error_message : `Page failed to open when clicking -> ${re.menu}`});
+            i++;
         }
+
+        this.activitySummary();
     }
 
     /**
      * Tests the menu in the header section
      */
     async checkHeadMenu() {
+        if( !this.page ) await this.startMenu();
+        console.log('TEST HEADER MENUS');
         let re;
         let head_list = ( this.menu_user )
                         ? this.menu_page.headExpectListLogin()
                         : this.menu_page.headExpectList();
-
+        let i = 0;
         for ( re of  head_list) {
-            console.log('TEST:',re);
-            await this.open(this.menu_page.head_menu, [re.menu], null, `Page failed to open when clicking -> ${this.menu_page.head_menu}`);
-            await this.open(re.menu, [re.expect], null, `Page failed to open when clicking -> ${re.menu}`);
+            console.log(`TEST ${i}:`,re);
+            await this.open(this.menu_page.head_menu, [re.menu], { idx: i, error_message : `Page failed to open when clicking -> ${this.menu_page.head_menu}`});
+            await this.open(re.menu, [re.expect], { idx: i, error_message : `Page failed to open when clicking -> ${re.menu}`});
+        i++;
         }
+
+        this.activitySummary();
     }
 }
-
-// Test
-let user = user_data[2]; // note that it should be a student because we are in katalkenglish.
-let katalk = new KatalkLoginPage; // extends menu
-let ontue = new OntueLoginPage;
-( new Menu( user, ontue ) ).main();
