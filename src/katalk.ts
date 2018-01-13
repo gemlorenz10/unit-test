880980a0ce69cf4906789ed95075f3465962b08b
gemlorenz10 <gemlorenz10@gmail.com>
2018-01-13 14:54:07 +0800

    divide into katalk.ts and ontue.ts

---
+++ src/katalk.ts
@@ -0,0 +1,11 @@
+﻿import { user_data } from './data/test-data';
+import { Register } from './scripts/register';
+import { KatalkLoginPage, KatalkRegistrationPage } from './scripts/lib/katalk-library';
+import { Menu } from './scripts/menu';
+// katalk pages
+let katalk_login_page = new KatalkLoginPage;
+let katalk_register_page = new KatalkRegistrationPage
+// students activities
+let student_eljei = user_data[1]; //student
+let menu_student = new Menu( student_eljei, katalk_login_page );
+let register_student = new Register( student_eljei, katalk_register_page );
---
+++ src/ontue.ts
@@ -0,0 +1,70 @@
+﻿import { Login } from './scripts/login';
+import { ISummary } from './scripts/lib/interface';
+import { OntueSchedule } from './scripts/ontue/ontue-crud-schedule';
+import { Menu } from './scripts/menu';
+
+import { OntueLoginPage, OntueRegistrationPage, OntueSchedulePage } from './scripts/lib/ontue-library';
+import { user_data, schedule_data } from './data/test-data';
+
+import { Register } from './scripts/register';
+import { PuppeteerExtension } from './puppeteer-extension';
+
+class Ontue extends PuppeteerExtension {
+}
+
+// test data
+let teacher_eden = user_data[0]; //teacher
+let schedule = schedule_data[0];
+//ontue pages
+let login_page = new OntueLoginPage;
+let register_page = new OntueRegistrationPage;
+let schedule_page = new OntueSchedulePage;
+
+// teachers activities
+let ontue = new Ontue;
+let login_teacher = new Login( teacher_eden, login_page )
+let register_teacher = new Register( teacher_eden, register_page );
+let menu_teacher = new Menu( teacher_eden, login_page );
+let schedule_teacher = new OntueSchedule( teacher_eden, schedule_page, schedule );
+
+let super_summary : ISummary= {
+    success : [],
+    js_error : [],
+    js_warn : [],
+    browser_error : [],
+    test_error : []
+}
+// populates global summary
+function addSummary( report: ISummary ) {
+
+    super_summary.success = super_summary.success.concat( report.success );
+    super_summary.js_error = super_summary.js_error.concat( report.js_error );
+    super_summary.js_warn = super_summary.js_warn.concat( report.js_warn );
+    super_summary.browser_error = super_summary.browser_error.concat( report.browser_error );
+    super_summary.test_error = super_summary.test_error.concat( report.test_error );
+
+}
+/**
+ * Run the script.
+ * @param script 
+ */
+async function run( script ){
+    await script.main().catch( async e => await script.error( e.code, e.message ) );
+    script.activitySummary( script.report );
+    addSummary( script.report );
+    await script.endScript();
+}
+
+(async function(){
+    // await run( register_teacher ).then( a => console.log('ONTUE REGISTRATION TESTING IS DONE.') );
+    // await run( login_teacher ).then( a => console.log('ONTUE LOGIN TESTING IS DONE.') );
+    await run( schedule_teacher ).then( a => console.log('ONTUE SCHEDULER TESTING IS DONE.') );
+    // await run( menu_teacher ).then( a => console.log('ONTUE MENU TESTING IS DONE.') );
+
+    ontue.activitySummary( super_summary );
+    await ontue.exitProgram(0);
+})().catch( async e => {
+    ontue.activitySummary( super_summary );
+    await ontue.fatal( e.code, e );
+});
+
