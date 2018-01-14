c118f188136588b06226eff9af6f45b1d558aa93
gemlorenz10 <gemlorenz10@gmail.com>
2018-01-14 20:53:11 +0800

    add reusable code to lib

--- src/ontue.ts
+++ src/ontue.ts
@@ -9,6 +9,7 @@ import { user_data, schedule_data } from './data/test-data';
 
 import { Register } from './scripts/register';
 import { PuppeteerExtension } from './puppeteer-extension';
+import * as util from './lib';
 
 class Ontue extends PuppeteerExtension {
 }
@@ -30,45 +31,20 @@ let menu_teacher = new Menu( teacher_eden, login_page );
 let schedule_teacher = new OntueSchedule( teacher_eden, schedule );
 let message_teacher = new OntueMessage( teacher_eden );
 
-let super_summary : ISummary= {
-    success : [],
-    js_error : [],
-    js_warn : [],
-    browser_error : [],
-    test_error : []
-}
-// populates global summary
-function addSummary( report: ISummary ) {
-
-    super_summary.success = super_summary.success.concat( report.success );
-    super_summary.js_error = super_summary.js_error.concat( report.js_error );
-    super_summary.js_warn = super_summary.js_warn.concat( report.js_warn );
-    super_summary.browser_error = super_summary.browser_error.concat( report.browser_error );
-    super_summary.test_error = super_summary.test_error.concat( report.test_error );
-
-}
-/**
- * Run the script.
- * @param script 
- */
-async function run( script ){
-    await script.main().catch( async e => await script.error( e.code, e.message ) );
-    script.activitySummary( script.report );
-    addSummary( script.report );
-    await script.endScript();
-}
-
 (async function(){
-    await run( register_teacher ).then( a => console.log('ONTUE REGISTRATION TESTING IS DONE.') );
-    await run( login_teacher ).then( a => console.log('ONTUE LOGIN TESTING IS DONE.') );
-    await run( schedule_teacher ).then( a => console.log('ONTUE SCHEDULER TESTING IS DONE.') );
-    await run( menu_teacher ).then( a => console.log('ONTUE MENU TESTING IS DONE.') );
-    await run( message_teacher ).then( a => console.log( 'ONTUE MESSAGE TESTING IS DONE.' ) );
 
-    ontue.activitySummary( super_summary, '************************SUPER SUMMARY************************' );
+    // await util.run( register_teacher ).then( a => console.log('ONTUE REGISTRATION TESTING IS DONE.') );
+    await util.run( login_teacher ).then( a => console.log('ONTUE LOGIN TESTING IS DONE.') );
+    // await util.run( schedule_teacher ).then( a => console.log('ONTUE SCHEDULER TESTING IS DONE.') );
+    // await util.run( menu_teacher ).then( a => console.log('ONTUE MENU TESTING IS DONE.') );
+    // await util.run( message_teacher ).then( a => console.log( 'ONTUE MESSAGE TESTING IS DONE.' ) );
+
+    ontue.activitySummary( util.super_summary, '************************ SUPER SUMMARY ************************', true );
     await ontue.exitProgram(0);
-})().catch( async e => {
-    ontue.activitySummary( super_summary );
-    await ontue.fatal( e.code, e );
-});
 
+})().catch( async e => {
+    
+    ontue.activitySummary( util.super_summary );
+    await ontue.fatal( e.code, e );
+
+});
