eb9cc46237799ef5b72a3c52ae682a94aa783bca
gemlorenz10 <gemlorenz10@gmail.com>
2018-01-18 19:47:38 +0800

    run katalk

--- src/katalk.ts
+++ src/katalk.ts
@@ -1,30 +1,45 @@
-﻿import { Login } from './scripts/login';
+﻿import { KatalkHome } from './scripts/katalk/katalk-home';
+import { Login } from './scripts/login';
 import { ISummary } from './scripts/lib/interface';
-import { user_data, schedule_data } from './data/test-data';
+import { user_data, schedule_data, student_data, teacher_data } from './data/test-data';
 import { Register } from './scripts/register';
-import { KatalkLoginPage, KatalkRegistrationPage } from './scripts/lib/katalk-library';
+import { KatalkLoginPage, KatalkRegistrationPage, KatalkHomePage } from './scripts/lib/katalk-library';
 import { Menu } from './scripts/menu';
 import * as util from './lib'
 import { PuppeteerExtension } from './puppeteer-extension';
+var argv = require('yargs').argv;
 
 class Katalk extends PuppeteerExtension{
 
 }
 let katalk = new Katalk;
 // katalk pages
-let katalk_login_page = new KatalkLoginPage;
-let katalk_register_page = new KatalkRegistrationPage
+let login_page = new KatalkLoginPage;
+let register_page = new KatalkRegistrationPage
+let home_page = new KatalkHomePage
 // students activities
-let student_eljei = user_data[1]; //student
-let student_menu = new Menu( katalk_login_page );
-let student_register = new Register( student_eljei, katalk_register_page );
-let student_login = new Login( student_eljei,  katalk_login_page );
 
-( async function(){
-    
-    await util.run( student_menu ).then( a => katalk.success( 'KATALK MENU TESTING DONE.' ) );
+global['login'] = new Login( student_data, login_page )
+global['register'] = new Register( student_data, register_page );
+global['menu'] = new Menu( login_page, student_data );
+global['menu-no-user'] = new Menu( login_page );
 
+// katalk testers
+global['home'] = new KatalkHome( student_data, home_page );
 
+let _global = [ global['login'], global['menu'], global['home'], global['menu-no-user'] ];
+let i, args = argv._[0];
+async function _run() {
+    if ( args ) { 
+        await util.run(global[args]); 
+    }
+    else {
+        for( i of _global ){
+            await util.run(i);
+        }
+    }
     katalk.activitySummary( util.super_summary, '************************ SUPER SUMMARY ************************', true );
     await katalk.exitProgram(0);
-})();
+}
+
+_run();
